c057619cac7099abaa506d90d443f7b87cd58a69
vadesxl <39384856+vadesxl@users.noreply.github.com>
2026-07-11 02:07:28 +0200

    Inject ABI symbol mangler explicitly

--- src/codegen.py
+++ src/codegen.py
@@ -4,9 +4,9 @@ from src import codegen_verified as _backend
 from src.abi import symbol
 from src.typesys import TypeChecker
 from src.ethics import SOURCES,SINKS,SANITIZERS
-_backend.symbol=symbol
 CodegenError=_backend.CodegenError
 class Codegen(_backend.Codegen):
+ def __init__(self):super().__init__(symbol_mangler=symbol)
  def generate(self,program):
   TypeChecker(set(SOURCES)|set(SINKS)|set(SANITIZERS)|{"print"}).check(program)
   return super().generate(program)
